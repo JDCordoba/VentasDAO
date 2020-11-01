1095f40594eb9df3207bc1c8534700047ed7e636
JDCordoba <juandc393@gmail.com>
2020-11-01 15:30:03 -0300

    deleted

--- VentasDAOV2.0/VentasDao/database/script02.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TABLE public.categorias
-(
-    id serial NOT NULL ,
-    denominacion character varying(60) COLLATE pg_catalog."default" NOT NULL,
-    descripcion character varying(80) COLLATE pg_catalog."default",
-    CONSTRAINT pk_id_categorias PRIMARY KEY (id)
-)
